18a125c15d7e824b2a108a80c6b1c3328b58d3d2
Leandro <klyte45@hotmail.com>
2018-09-23 19:44:58 -0300

    v2.0.2

--- TouchThisToolMod.cs
+++ TouchThisToolMod.cs
@@ -11,7 +11,7 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
-[assembly: AssemblyVersion("2.0.1.*")]
+[assembly: AssemblyVersion("2.0.2.0")]
 namespace Klyte.TouchThis
 {
     public class TouchThisToolMod : MonoBehaviour, IUserMod, ILoadingExtension
