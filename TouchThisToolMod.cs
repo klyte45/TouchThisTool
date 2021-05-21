0d96fb180708d2510b9cc6e374672cc8dcb8d7a5
Leandro Klyte <klyte45@hotmail.com>
2021-05-21 04:13:40 -0300

    v4.0.0r6 Removing dialog fallback

--- TouchThisToolMod.cs
+++ TouchThisToolMod.cs
@@ -2,7 +2,7 @@ using ICities;
 using Klyte.Commons.Interfaces;
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.0.0.5")]
+[assembly: AssemblyVersion("4.0.0.6")]
 namespace Klyte.TouchThis
 {
     public class TouchThisToolMod : BasicIUserMod<TouchThisToolMod, TTTController, TTTPanel>
