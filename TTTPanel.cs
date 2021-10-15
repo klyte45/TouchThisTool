e24b788bf3f4a6aaaea23b6b8d96605ac86f0cef
Leandro Klyte <klyte45@hotmail.com>
2021-10-15 13:34:49 -0300

    Start of implementation of TTT5 as Upgrade Untouchable Tool

--- TTTPanel.cs
+++ TTTPanel.cs
@@ -1,3 +1,4 @@
+using ColossalFramework.Globalization;
 using ColossalFramework.UI;
 using Klyte.Commons.Extensions;
 using Klyte.Commons.Interfaces;
@@ -8,12 +9,14 @@ namespace Klyte.TouchThis
 {
     public class TTTPanel : BasicKPanel<TouchThisToolMod, TTTController, TTTPanel>
     {
-        public override float PanelWidth => 400;
+        public override float PanelWidth => 500;
 
-        public override float PanelHeight => 230;
+        public override float PanelHeight => 260;
 
         private InfoManager.InfoMode m_currentMode = InfoManager.InfoMode.None;
         private InfoManager.SubInfoMode m_currentSubMode = InfoManager.SubInfoMode.None;
+
+        internal UILabel m_currentlyUpgradingLabel;
 
         protected override void AwakeActions()
         {
@@ -26,7 +29,14 @@ namespace Klyte.TouchThis
             layoutPanel.autoLayoutPadding = new RectOffset(0, 0, 10, 10);
             var uiHelper = new UIHelperExtension(layoutPanel);
 
-            uiHelper.AddCheckboxLocale("K45_TTT_SHOWUNDERGROUNDVIEW", false, OnUndergroundChange);
+            KlyteMonoUtils.LimitWidthAndBox(uiHelper.AddCheckboxLocale("K45_TTT_SHOWUNDERGROUNDVIEW", false, OnUndergroundChange).label, PanelWidth - 40); ;
+            var classicMode = uiHelper.AddCheckboxLocale("K45_TTT_CLASSICTOUCHTHISMODE", false, OnClassicModeChanged);
+            classicMode.label.processMarkup = true;
+            KlyteMonoUtils.LimitWidthAndBox(classicMode.label, PanelWidth - 40);
+
+            m_currentlyUpgradingLabel = uiHelper.AddLabel("\n\t");
+            m_currentlyUpgradingLabel.prefix = Locale.Get("K45_TTT_CURRENTLYUPGRADINGTO");
+            KlyteMonoUtils.LimitWidthAndBox(m_currentlyUpgradingLabel, PanelWidth - 10);
         }
 
         private void OnUndergroundChange(bool isChecked)
@@ -35,6 +45,12 @@ namespace Klyte.TouchThis
             m_currentSubMode = isChecked ? InfoManager.SubInfoMode.UndergroundTunnels : InfoManager.SubInfoMode.None;
             OnShow(MainPanel.isVisible);
         }
+        private void OnClassicModeChanged(bool isChecked)
+        {
+            TouchThisTool.instance.SetClassicMode(isChecked);
+            m_currentlyUpgradingLabel.isVisible = !isChecked;
+        }
+
         private void OnShow(bool value) => TouchThisToolMod.Controller?.ToggleTool(value, m_currentMode, m_currentSubMode);
     }
 }
